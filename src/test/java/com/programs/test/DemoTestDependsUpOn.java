532637fa14e65c2847859014f693639a601147a8
AjithB1995 <bandarilaxmi05@gmail.com>
2021-11-29 21:29:33 +0530

    Framework Design

--- src/test/java/com/programs/test/DemoTestDependsUpOn.java
+++ src/test/java/com/programs/test/DemoTestDependsUpOn.java
@@ -1,12 +1,7 @@
 package com.programs.test;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+import static org.testng.Assert.fail;
+
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
@@ -69,6 +64,7 @@ public class DemoTestDependsUpOn {
 	@Test
 	public void test3() {
 		System.out.println("Test3 Method Executed");
+		//fail("failed test3");
 	}
 		
 	@Test(dependsOnMethods = {"test1"})
